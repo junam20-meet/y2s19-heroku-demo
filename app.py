45840e53fd5436409c5196be4d4b8a4f0440696e
myname21-meet <myname21@meet.mit.edu>
2019-07-31 11:22:23 -0400

    gg

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from flask import Flask, render_template, url_for, request
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World test!'
+	return render_template('home.html', students=query_all())
+    # return 'Hello, World test!'
 
 if __name__ == '__main__':
     app.run(debug=True)
